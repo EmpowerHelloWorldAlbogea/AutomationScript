f937b18875ed6b7f54890433cd2d355d8306a649
albogea <albogea@STDENGCOM-ALBOG>
2023-05-30 16:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("03295B33-C4EC-46AB-AA12-80F51FF558EC")]
+[assembly: AssemblyVersion("1.0.0.0")]
